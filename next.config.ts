4b05965919293fdd36b94b6234444a75cdfcc030
Aaron Brazell <aaron@technosailor.com>
2025-04-24 13:38:11 -0400

    add d3

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  target: 'server',
+  generateBuildId: () => null,
+  port: process.env.PORT || 3000,
+  disableServeStaticFilesDuringDevelopment: false,
 };
 
 export default nextConfig;
